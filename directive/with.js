1ae2e5066993aeaaf7fd76c8fbcd06bb8dd38f0a
Dmitry Iv <df.creative@gmail.com>
2024-05-18 16:45:36 -0400

    Make single with effect run

--- directive/with.js
+++ directive/with.js
@@ -3,11 +3,14 @@ import store, { _signals } from '../store.js';
 import { effect } from "../signal.js";
 
 directive.with = (el, evaluate, rootState) => {
-  const state = sprae(el,
-    store(
-      evaluate(rootState),
-      Object.create(rootState[_signals])
-    )
-  );
-  return effect(() => Object.assign(state, evaluate(rootState)))
+  let state, values
+  return effect(() => {
+    values = evaluate(rootState);
+    Object.assign(state ||= sprae(el,
+      store(
+        values,
+        Object.create(rootState[_signals])
+      )
+    ), values)
+  })
 };
