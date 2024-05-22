9585689558b58c0013da3614bfd58d80c4db22fd
Dmitry Iv <df.creative@gmail.com>
2024-05-22 06:03:15 -0400

    Simplify ref

--- directive/ref.js
+++ directive/ref.js
@@ -1,11 +1,10 @@
-import { signal } from "../signal.js";
 import { directive } from "../core.js";
 import { _change, _signals } from "../store.js";
 import { ipol } from './default.js';
 
 // ref must be last within primaries, since that must be skipped by :each, but before secondaries
 directive.ref = (el, expr, state) => {
-  state[_signals][ipol(expr, state)] = signal(el)
+  state[ipol(expr, state)] = el
 }
 
 directive.ref.parse = expr => expr
