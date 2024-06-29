7df54cb28bb5c0567b0c0029ba3fc22878ba7eef
Dmitry Iv <df.creative@gmail.com>
2024-06-29 14:31:06 -0400

    Simplify if handler

--- directive/if.js
+++ directive/if.js
@@ -5,37 +5,36 @@ import { effect } from "../signal.js";
 // as for :if :with - :if must init first, since it is lazy, to avoid initializing component ahead of time by :with
 // we consider :with={x} :if={x} case insignificant
 const _prevIf = Symbol("if");
-directive.if = (ifEl, evaluate, state) => {
-  let next = ifEl.nextElementSibling,
+directive.if = (el, evaluate, state) => {
+  let next = el.nextElementSibling,
     holder = document.createTextNode(''),
 
     // actual replaceable els (takes <template>)
-    none = [], cur = none, ifs, elses;
+    cur, _if, _else;
 
-  ifEl.replaceWith(holder)
+  el.replaceWith(holder)
 
-  ifs = ifEl.content ? [frag(ifEl)] : [ifEl]
+  _if = el.content ? frag(el) : el
 
   if (next?.hasAttribute(":else")) {
     next.removeAttribute(":else");
     // if next is :else :if - leave it for its own :if handler
-    if (next.hasAttribute(":if")) elses = none;
-    else next.remove(), elses = next.content ? [frag(next)] : [next];
-  } else elses = none;
+    if (!next.hasAttribute(":if")) next.remove(), _else = next.content ? frag(next) : next;
+  }
 
   // we mark all els as fake-spraed, because we have to sprae for real on insert
-  for (let el of [...ifs, ...elses]) memo.set(el, null)
+  memo.set(_if, null)
+  if (_else) memo.set(_else, null)
 
   return effect(() => {
-    const newEls = evaluate(state) ? ifs : ifEl[_prevIf] ? none : elses;
-    if (next) next[_prevIf] = newEls === ifs
-    if (cur != newEls) {
-      for (let el of cur) el.remove();
-      cur = newEls;
-      for (let el of cur) {
-        holder.before(el.content || el)
-        memo.get(el) === null && memo.delete(el) // remove fake memo to sprae as new
-        sprae(el, state)
+    const newEl = evaluate(state) ? _if : el[_prevIf] ? null : _else;
+    if (next) next[_prevIf] = newEl === _if
+    if (cur != newEl) {
+      cur?.remove()
+      if (cur = newEl) {
+        holder.before(cur.content || cur)
+        memo.get(cur) === null && memo.delete(cur) // remove fake memo to sprae as new
+        sprae(cur, state)
       }
     }
   });
