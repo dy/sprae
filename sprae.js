bb9824cd1407aefba3debe7b36710ec46a48840d
Dmitry Iv <df.creative@gmail.com>
2024-10-22 06:02:43 -0400

    Remove html

--- sprae.js
+++ sprae.js
@@ -5,13 +5,14 @@ import './directive/if.js'
 import './directive/each.js'
 import './directive/ref.js'
 import './directive/with.js'
-import './directive/html.js'
 import './directive/text.js'
 import './directive/class.js'
 import './directive/style.js'
 import './directive/value.js'
 import './directive/fx.js'
 import './directive/default.js'
+import './directive/aria.js'
+import './directive/data.js'
 
 // default compiler (indirect new Function to avoid detector)
 sprae.use({ compile: expr => sprae.constructor(`with (arguments[0]) { return ${expr} };`) })
