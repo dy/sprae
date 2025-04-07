8a670d36bdc3e50f5468c75ca53bfa87a9f64589
Dmitry Iv. <df.creative@gmail.com>
2025-04-07 19:27:10 -0400

    Make generic parts in core

--- directive/ref.js
+++ directive/ref.js
@@ -1,9 +1,11 @@
-import { dir } from "../core.js";
+import { dir, parse } from "../core.js";
 import { setter, ensure } from "../store.js";
 
-dir('ref', (el, state, expr, _, ev) => (
-  ensure(state, expr),
-  ev(state) == null ?
+dir('ref', (el, state, expr) => {
+  // ensure state has first element of path
+  ensure(state, expr)
+  // it's ok to call parse separately, since it caches expr
+  return parse(expr)(state) == null ?
     (setter(expr)(state, el), _ => _) :
     v => v.call(null, el)
-))
+})
